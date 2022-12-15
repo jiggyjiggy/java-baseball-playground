77587fac41c8d09ff591298fce437df2dc64ce6b
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-12-15 15:36:14 +0900

    test(SetTest): add contains true false test by CsvSource

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
@@ -36,7 +38,18 @@ public class SetTest {
     @DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인")
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
-    void contains(int number) {
+    void contains_test1(int number) {
         assertTrue(numbers.contains(number));
+    }
+
+    @DisplayName("위의 contains_test1은 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. " +
+            "입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.\n" +
+            "예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, " +
+            "4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains_test2(int number, boolean expected) {
+        boolean actualValue = numbers.contains(number);
+        assertEquals(expected, actualValue);
     }
 }
