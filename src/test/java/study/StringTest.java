eeaedfbea9a1c7888c3685e70aad32dfe4570547
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-12-15 14:34:37 +0900

    test(요구사항1): add split test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,4 +11,18 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    @DisplayName("\"1,2\"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인")
+    void split_test() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1", "2");
+    }
+    @Test
+    @DisplayName("\"1\"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인")
+    void split_test2() {
+        String[] actual = "1,".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+    
 }
