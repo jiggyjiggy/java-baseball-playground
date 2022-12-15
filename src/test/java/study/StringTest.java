2d1d3da654f414c1ed78cf2c8020b8e8b234742b
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-12-15 14:57:20 +0900

    test(요구사항3): add charAt test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
@@ -24,11 +25,33 @@ public class StringTest {
         String[] actual = "1,".split(",");
         assertThat(actual).containsExactly("1");
     }
-    
+
     @Test
     @DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 \"1,2\"를 반환")
     void substring_test() {
         String actual = "(1,2)".substring(1, 4);
         assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습")
+    void charAt_test1() {
+        String target = "abc";
+        assertThat(target.charAt(0)).isEqualTo('a');
+        assertThat(target.charAt(1)).isEqualTo('b');
+        assertThat(target.charAt(2)).isEqualTo('c');
+    }
+    
+    /*
+    * The assertThatExceptionOfType method allows you to test whether an exception of a specific type is thrown, while the assertThatThrownBy method allows you to test whether a piece of code throws any exception at all.
+    * */
+    @Test
+    @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 " +
+            "StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습")
+    void charAt_test2() {
+        String target = "abc";
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> target.charAt(4))
+                .withMessageMatching("String index out of range: \\d+");
     }
 }
