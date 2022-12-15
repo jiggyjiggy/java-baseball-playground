7d6cfabc90535135abb015168c9ebd9fe892c4e5
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-12-15 15:14:11 +0900

    test(SetTest): add size test

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,9 +1,13 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -18,4 +22,11 @@ public class SetTest {
     }
 
     // Test Case 구현
+
+    @Test
+    @DisplayName("Set의 size() 메소드를 활용해 Set의 크기를 확인")
+    void size_test() {
+        int actual = numbers.size();
+        assertThat(actual).isEqualTo(3);
+    }
 }
