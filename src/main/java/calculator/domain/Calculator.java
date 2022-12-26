cbc3edfc9b7257fb92e0d2496cdad9faea8be6ee
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-12-26 16:30:48 +0900

    feat(calculator): create domain

---
+++ src/main/java/calculator/domain/Calculator.java
@@ -0,0 +1,16 @@
+package calculator.domain;
+
+public class Calculator {
+    public int add(int x, int y) {
+        return x + y;
+    }
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+    public int times(int x, int y) {
+        return x * y;
+    }
+    public int divide(int x, int y) {
+        return x / y;
+    }
+}
